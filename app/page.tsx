3efbd581c49544623ee2e0fcceb51913c06d5ccc
pushpendra <pushpendrathakur145@gmail.com>
2026-01-20 12:36:44 +0530

    setup auto deploy

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="flex flex-col justify-start items-center py-20 w-screen min-h-screen bg-red-100">
+   <div className="flex flex-col justify-start items-center py-20 w-screen min-h-screen bg-green-100">
             <TodoForm/>
             <hr className="bg-gray-600 w-[90%] h-[2px] my-5"/>
             <TodoList/>
